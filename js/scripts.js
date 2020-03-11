944cae0bb0e5072bef36c595dee982ba25e76575
Gustavo Piá <piagja@gmail.com>
2020-03-10 21:25:19 -0300

    adicionaa imagens de ondas para a tela principal

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,20 @@ input.forEach(input => {
 })
 
 // fim do código do efeito para o login.html
+
+// index.html efeitos do menu ativo
+
+
+// pegando elementos com arrow function
+const element = (el) => document.querySelector(el)
+
+/* abrindo */
+element('.open').addEventListener('click', () => {
+  element('.nav-list').classList.add('active')
+})
+
+
+/* fechando */
+element('.close').addEventListener('click', () => {
+  element('.nav-list').classList.remove('active')
+})
